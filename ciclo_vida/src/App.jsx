84cf520af2c6be3c8577b00a1c22673ad8574f66
Rivi03 <riosschrodinger@gmail.com>
2025-06-28 20:56:26 -0600

    feat: Refactor App component to implement navigation state and enhance functionality

--- ciclo_vida/src/App.jsx
+++ ciclo_vida/src/App.jsx
@@ -1,33 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState, useEffect, useMemo, use } from 'react';
+import Planeta from './Planeta';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [distancia, setDistancia] = useState(0)
+  const [combustible, setCombustible] = useState(0)
+  const [estado_nave, setEstadoNave] = useState('En Órbita')
+  const [planetasVisitados, setPlanetasVisitados] = useState([])
+  const [mostrarbitacora, setMostrarBitacora] = useState(false)
 
+  useEffect(() => {
+    console.log('El panel de control se ha encendido');
+
+    const intervalo = setInterval(() => {
+
+      if (combustible > 0 && estado_nave === 'En Órbita') {
+        setDistancia(prev => prev + 1);
+        setCombustible(prev => prev - 1);
+      }else if (combustible <= 0) {
+          setEstadoNave("Sin combustible");
+        }
+      },100);
+
+        return () => {
+          clearInterval(intervalo);
+          console.log('El panel de control se ha apagado');
+        };},[combustible, estado_nave]);
+
+        useEffect(() => {
+          console.log('Combustible actualizado:');},[combustible]);
+
+        const mensajeEstado = useMemo(() => {
+          return `Estado de la nave: ${estado_nave}`;}, [estado_nave]);
+
+        const aterrizar = () => {
+          const nombrePlaneta = `Planeta-${distancia}`;
+          setEstadoNave('Aterrizando');
+          setPlanetasVisitados(prev => [...planetasVisitados, nombrePlaneta]);
+
+          setTimeout(() => {
+            setEstadoNave('En Órbita');
+            setCombustible(prev => Math.min(prev + 30), 100);
+          }, 2000);
+        };
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
